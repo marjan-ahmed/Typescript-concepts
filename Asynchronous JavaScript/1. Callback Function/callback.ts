d063fd08f82b34537995d89570631004b1b9a1f7
marjan-ahmed <marjan.ahmed08@yahoo.com>
2024-07-16 22:33:07 +0500

    about callback: callback.ts

---
+++ Asynchronous JavaScript/1. Callback Function/callback.ts
@@ -0,0 +1,15 @@
+// A function that takes a parameter and arguments of another function is callback function
+
+// Expect callback function 
+function parentFunc(func: () => void): void {
+    console.log("I am a parent function");
+    func();
+}
+
+// Calling the parent function with a callback function
+function childFunc() {
+    console.log("I am a child function");
+}
+
+parentFunc(childFunc); 
+
